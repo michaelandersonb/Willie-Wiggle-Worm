e3eb07ba61cb04d63e377995fc6e6bbfcd9652df
Michael Anderson <michaelandersonb@gmail.com>
2022-09-18 14:15:55 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "34px serif";
+ctx.textAlign = "center";
+ctx.textBaseline="middle"; 
+ctx.fillStyle = "#FFF";
+ctx.fillText("Hello World",150,50);
